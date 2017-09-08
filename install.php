5b486e2c42026939baa764e4a7c63916592390b6
schuer <ds@decaf.de>
2017-09-08 11:50:01 +0200

    update install/uninstall process, check for arrays

--- install.php
+++ install.php
@@ -2,13 +2,30 @@
 
 /** @var rex_addon $this */
 
+// copy backup files
+rex_dir::copy(
+    $this->getPath('backups'),
+    rex_addon::get('backup')->getDataPath()
+);
+
+
 // read configs
 $config1 = rex_file::getConfig($this->getPath('package.yml'));
 $config2 = rex_file::getConfig($this->getPath('package.setup.yml'));
 
-// merge packages and requires sections
-$packages = $config1['requires']['packages'] + $config2['requires']['packages'];
-$requires = $config1['requires'] + $config2['requires'];
+$requires1 = $config1['requires'];
+$requires2 = $config2['requires'];
+$packages1 = $config1['requires']['packages'];
+$packages2 = $config2['requires']['packages'];
+
+if (!is_array($requires1)) $requires1 = array();
+if (!is_array($requires2)) $requires2 = array();
+if (!is_array($packages1)) $packages1 = array();
+if (!is_array($packages2)) $packages2 = array();
+
+// merge requires and packages sections
+$requires = $requires1 + $requires2;
+$packages = $packages1 + $packages2;
 
 // merge configs
 $config = array_merge(
@@ -17,14 +34,8 @@ $config = array_merge(
 );
 
 // update config with requires and packages
-$config['requires'] = $requires;
-$config['requires']['packages'] = $packages;
+if (count($requires) > 0) $config['requires'] = $requires;
+if (count($packages) > 0) $config['requires']['packages'] = $packages;
 
 // save config
 rex_file::putConfig($this->getPath('package.yml'), $config);
-
-// copy backup files
-rex_dir::copy(
-    $this->getPath('backups'),
-    rex_addon::get('backup')->getDataPath()
-);
--- uninstall.php
+++ uninstall.php
@@ -6,14 +6,24 @@
 $config1 = rex_file::getConfig($this->getPath('package.yml'));
 $config2 = rex_file::getConfig($this->getPath('package.setup.yml'));
 
+$packages1 = $config1['requires']['packages'];
+$packages2 = $config2['requires']['packages'];
+
+if (!is_array($packages1)) $packages1 = array();
+if (!is_array($packages2)) $packages2 = array();
+
 // reduce packages to initial version
 $packages = array_diff(
-    $config1['requires']['packages'],
-    $config2['requires']['packages']
+    $packages1,
+    $packages2
 );
 
-// update config with initial packages
-$config1['requires']['packages'] = $packages;
+// update config with initial packages or delete if empty
+if (count($packages) > 0) {
+    $config1['requires']['packages'] = $packages;
+} else {
+    unset($config1['requires']['packages']);
+}
 
 // remove setup data
 unset($config1['setup']);
